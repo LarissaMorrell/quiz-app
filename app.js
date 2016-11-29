8b75f22de1f62ac392ad8565a2ec4259b077c44e
LarissaMorrell <larissa@larissamorrell.com>
2016-11-29 18:23:33 -0500

    updated render to include questNum and question

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 var problem = {}
 
 
@@ -16,10 +15,15 @@ var addQuestion = function(questNum, quest, ans, ansIndex) {
 
 
 //Render functions
-var displayQuestion = function(problem, element) {
+var displayQuestion = function(problem, form) {
 
+    form.children('.js-question-number').text('Question ' + problem.questNumber + ' of 10');
+    form.children('.js-question').text(problem.question);
 
-    element.html('<p class="question-number">Question ' + problem.questNum + ' of 10</p>');
+    //now add the answers as a generated HTML string from getAnswers(problem)
+    $(getAnswers(problem)).insertAfter(form.children('js-question'));
+
+    $('.right-wrong').hide();
 
     /**<p class="question-number">Question 1 of 10</p>
                 <p class="question" value="0"></p>
@@ -41,7 +45,18 @@ var displayQuestion = function(problem, element) {
 }
 
 
+var getAnswers = function(problem) {
 
+    var answerString = "";
+
+    for (var i = 0; i < 4; i++) {
+
+        answerString += '<input type="radio" value="' + i +
+            '">' + problem.answers[i] + '<p class="right-wrong"></p><br>';
+    }
+
+    return answerString;
+}
 
 
 
@@ -51,16 +66,15 @@ var displayQuestion = function(problem, element) {
 //Event Listeners
 
 $(document).ready(function() {
-	var questionNum = 1;
+    var questionNum = 1;
 
     addQuestion(questionNum, 'How tall is Mt. Washington?', ["4,981'", "5,829'", "6,289'", "7,011'"], 3);
     displayQuestion(problem, $('.js-q-area'));
 
-    $('button').on('click', function(event) {
+    $('button').on('click', function(event) {   //be more specific w button later
         event.preventDefault();
 
-        
-
         questionNum++;
+        //trigger addQuestion() and displayQuestion()
     });
 });
