40a924719c13270e84fd0c67a9ab9954fc090e9e
LarissaMorrell <larissa@larissamorrell.com>
2016-12-03 15:07:39 -0500

    updated user feedback after response submitted

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var questions = []; //array of question# objects
-
+var pointsEarned = 0;
 
 //State modification functions
 function Question(prompt, answers, correctAnswerIndex) {
@@ -14,7 +14,7 @@ var question1 = new Question('How tall is Mt. Washington?', ["4,981'", "5,829'",
 var question2 = new Question('Up until 1996, Mt. Washington has had the highest recorded wind speeds on Earth. What was the speed?', ['189 mph', '231 mph', '295 mph', '305 mph'], 1);
 var question3 = new Question('In 1524, Giovanni da Verrazzano was the first European to mention the mountain. He was viewing it from which location?', ['Plymouth Rock', 'Mt. Adams', 'Mt. Katahdin', 'The Atlantic Ocean'], 3);
 var question4 = new Question('The most popular trail up the mountain is also the most deadly. What is the name of this trail?', ['Tuckerman Ravine Trail', 'Appalachian Trail', 'Lion Head Trail', 'Boott Spur Trail'], 0);
-var question5 = new Question('What reason makes Tuckerman Ravine Trail the most deadly?', ['It is a family trail, which has many unprepared hikers.', 'The rapidly changing weather conditions.', 'The area is prone to avalanches.', 'All of the above.'], 0);
+var question5 = new Question('What reason makes Tuckerman Ravine Trail the most deadly?', ['It is a family trail, which has many unprepared hikers.', 'The rapidly changing weather conditions.', 'The area is prone to avalanches.', 'All of the above.'], 3);
 
 questions.push(question1);
 questions.push(question2);
@@ -24,32 +24,6 @@ questions.push(question5);
 
 
 
-
-function nextQuestion(questionNum) {
-
-
-    displayQuestion(questionNum);
-
-    if (questions[questionNum].currentlyDisplayed && questionNum < 4) {
-
-        questions[questionNum].currentlyDisplayed = false;
-        questions[questionNum + 1].currentlyDisplayed = true;
-
-        //render funtion
-
-    } else if (questionNum == 4) {
-        //end the quiz with final score
-
-
-    } else { //currently displayed is false so we are at quest 0
-
-        questions[questionNum].currentlyDisplayed = true;
-
-        //render function
-        //displayQuestion(questions[questionNum]);
-    }
-
-}
 
 function checkAnswer(choice, questionNum) {
 
@@ -84,8 +58,6 @@ function displayQuestion(questionNum) {
     //must 
     $('.js-answers').append(getChoices(questionNum));
 
-    $('.right-wrong').hide();
-
     //loop through the questions and display currentlyDidsplayed=true
 
 }
@@ -99,7 +71,7 @@ function getChoices(questionNum) {
     for (var i = 0; i < 4; i++) {
 
         answerString += '<li><input type="radio" name="choice" value="' + i +
-            '">  ' + questions[questionNum].answers[i] + '<p class="right-wrong"></p></li>';
+            '">  ' + questions[questionNum].answers[i] + '</li>';
     }
 
     return answerString;
@@ -107,6 +79,31 @@ function getChoices(questionNum) {
 
 
 
+function userSubmitFeedback(questionNum) {
+    //take in the numeric numbered radio button, and set equal to choice
+    var choice = $('input[name=choice]:checked').val()
+
+    //check to make sure the correct answer has been selected
+    if (checkAnswer(choice, questionNum)) {
+
+        //toggle buttons
+        $('.submit').hide();
+        $('.next').show();
+
+        //show the correct answer and the score
+        $('.wrong').remove(); //remove any previous feedback from wrong answers
+        $('<p class="right">Correct!</p>').insertBefore('.progress');
+
+        return true;
+
+    } else { //if the children of the form have the class of .wrong
+
+        if (!$('p').hasClass('wrong')) {
+            $('<p class="wrong">Try again.</p>').insertBefore('.progress');
+        }
+        return false;
+    };
+}
 
 
 
@@ -127,27 +124,22 @@ $(document).ready(function() {
     $('.submit').on('click', function(event) {
         event.preventDefault();
 
+        //check for end of quiz
+        if (questionNum == questions.length - 1) {
+            $('.js-q-area').children().remove();
 
-        //take in the numeric numbered radio button, and set equal to choice
-        //but this won't work because radios are not there on page load
-        var choice = $("input[name=choice]:checked").val()
-
-        //check to make sure an answer has been selected
-        if (checkAnswer(choice, questionNum)) {
-            submitted = true;
-
-            //toggle buttons
-            $('.submit').hide();
-            $('.next').show();
-
-           //show the correct answer and the score
         }
+
+        submitted = userSubmitFeedback(questionNum);
+
     });
 
 
-//input[name='next-question']
+    //input[name='next-question']
     $('.next').on('click', function(event) { //be more specific w button later
         event.preventDefault();
+
+        $('.right').remove();
 
         //toggle buttons
         $('.submit').show();
@@ -155,7 +147,7 @@ $(document).ready(function() {
 
         if (submitted) {
             //move on to next question
-            nextQuestion(++questionNum);
+            displayQuestion(++questionNum);
 
             submitted = false; //reset
         }
